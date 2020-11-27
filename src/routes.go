0761dcf0d071081a887dc47140f7e42d9ecb621f
Chane <Chane@procentrica.co.za>
2020-11-27 11:37:42 +0200

    Commit for GCP

--- src/routes.go
+++ src/routes.go
@@ -2,4 +2,6 @@ package main
 
 func (s *Server) routes() {
 	s.router.HandleFunc("/verifycred", s.verifycredentials()).Methods("POST")
+	s.router.HandleFunc("/registeruser", s.handleregisteruser()).Methods("POST")
+	s.router.HandleFunc("/assigngroup", s.handleassigngroup()).Methods("GET")
 }
